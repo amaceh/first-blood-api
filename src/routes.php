7e3ee9ca4f32659c62a39eb258d1b24c6eea3a40
amaceh <achmadar9i@gmail.com>
2018-04-28 09:31:35 +0700

    add posting route

--- src/routes.php
+++ src/routes.php
@@ -17,30 +17,30 @@ $app->get("/users/login/", function(Request $request, Response $response){
     $user = $request->getQueryParam("user");
     $pass = $request->getQueryParam("pass");
 
-	$sql = "SELECT * FROM fsbld_users WHERE username=:username OR email=:email";
+    $sql = "SELECT * FROM fsbld_users WHERE username=:username OR email=:email";
     $stmt = $this->db->prepare($sql);
     $stmt->execute([":username"=>$user,":email"=>$user]);
     $result = $stmt->fetchAll();
     if ($stmt->rowCount() > 0) {
-    	if (password_verify($pass, $result[0]["password"])){
-    		$sql = "UPDATE fsbld_users SET api_key=:api_key WHERE username=:username OR email=:email";
-		    $stmt = $this->db->prepare($sql);
-		    $new_api_key = bin2hex(random_bytes(32));
-		    $data = [
+        if (password_verify($pass, $result[0]["password"])){
+            $sql = "UPDATE fsbld_users SET api_key=:api_key WHERE username=:username OR email=:email";
+            $stmt = $this->db->prepare($sql);
+            $new_api_key = bin2hex(random_bytes(32));
+            $data = [
                 ":username"=>$user,
                 ":email"=>$user,
-		        ":api_key" => $new_api_key
-		    ];
-   			if($stmt->execute($data)){
+                ":api_key" => $new_api_key
+            ];
+            if($stmt->execute($data)){
                 $result[0]['api_key']=$new_api_key;
-    			return $response->withJson(["status" => "success", "data" => $result[0]], 200);
-			}else
-		    	return $response->withJson(["status" => "failed", "data" => NULL], 200);
-    	}else
-	    	return $response->withJson(["status" => "wrong password", "data" => NULL], 200);
+                return $response->withJson(["status" => "success", "data" => $result[0]], 200);
+            }else
+                return $response->withJson(["status" => "failed", "data" => NULL], 200);
+        }else
+            return $response->withJson(["status" => "wrong password", "data" => NULL], 200);
     }
     else
-    	return $response->withJson(["status" => "failed", "data" => NULL], 200);
+        return $response->withJson(["status" => "failed", "data" => NULL], 200);
 });
 
 $app->post("/users/register/", function (Request $request, Response $response){
@@ -90,3 +90,126 @@ $app->put("/users/update/", function (Request $request, Response $response){
     
     return $response->withJson(["status" => "failed", "data" => "0"], 200);
 });
+
+$app->get("/users/getUsers/", function(Request $request, Response $response){
+    $sql = "SELECT (username, email, nama, goldar, rhesus, no_hp, foto_profil) FROM fsbld_users";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    if ($stmt->rowCount() > 0) {    
+        if($stmt->execute($data)){
+            $result[0]['api_key']=$new_api_key;
+            return $response->withJson(["status" => "success", "data" => $result[0]], 200);
+        }else
+            return $response->withJson(["status" => "failed", "data" => NULL], 200);
+    }
+    else
+        return $response->withJson(["status" => "failed", "data" => NULL], 200);
+});
+
+///////////////////////////////////////////////////////////////////////////////
+
+$app->get("/posting/", function (Request $request, Response $response){
+    $sql = "SELECT * FROM posts";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->get("/posting/latest/", function (Request $request, Response $response){
+    // for syncing purpose
+    //SELECT * FROM fsbld_post WHERE inserted_at > '2018-04-28 08:00:00'
+    $waktu = $request->getQueryParam("time");
+    $sql = "SELECT * FROM posts WHERE inserted_at > :waktu";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute([":waktu"=>$waktu]);
+    //$stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->get("/posting/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $sql = "SELECT * FROM posts WHERE id_post=:id";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->get("/posting/search/", function (Request $request, Response $response, $args){
+    $keyword = $request->getQueryParam("keyword");
+    $sql = "SELECT * FROM posts WHERE id_post LIKE '%$keyword%' OR username LIKE '%$keyword%' OR goldar LIKE '%$keyword%' OR rhesus LIKE '%$keyword%' OR descrip LIKE '%$keyword%' OR rumah_sakit LIKE '%$keyword%' OR status LIKE '%$keyword%' OR inserted_at LIKE '%$keyword%' OR updated_at LIKE '%$keyword%'";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+    //var_dump($sql);
+});
+
+$app->post("/posting/", function (Request $request, Response $response){
+
+    $new_mk = $request->getParsedBody();
+
+    $sql = "INSERT INTO posts (username, goldar, rhesus, descrip, rumah_sakit, status, inserted_at, updated_at)"; 
+    $sql .= "VALUE (:username, :goldar, :rhesus, :descrip, :rumah_sakit, :status, :inserted_at, :updated_at)";
+    $stmt = $this->db->prepare($sql);
+
+    $data = [
+        ":username" => $new_mk["username"], 
+        ":goldar" => $new_mk["goldar"], 
+        ":rhesus" => $new_mk["rhesus"], 
+        ":descrip" => $new_mk["descrip"], 
+        ":rumah_sakit" => $new_mk["rumah_sakit"], 
+        ":status" => $new_mk["status"], 
+        ":inserted_at" => $new_mk["inserted_at"], 
+        ":updated_at" => $new_mk["updated_at"]
+    ];
+
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
+});
+
+
+$app->put("/posting/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $new_mk = $request->getParsedBody();
+    $sql = "UPDATE posts SET goldar=:goldar, rhesus=:rhesus, descrip=:descrip, rumah_sakit=:rumah_sakit, status=:status, updated_at=:updated_at WHERE id_post=:id";
+    $stmt = $this->db->prepare($sql);
+    
+    //tidak semua attribute perlu diupdate
+    $data = [
+        ":id_post" => $new_mk["id_post"],
+        ":goldar" => $new_mk["goldar"], 
+        ":rhesus" => $new_mk["rhesus"], 
+        ":descrip" => $new_mk["descrip"], 
+        ":rumah_sakit" => $new_mk["rumah_sakit"], 
+        ":status" => $new_mk["status"], 
+        ":updated_at" => $new_mk["updated_at"]
+    ];
+
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
+});
+
+
+$app->delete("/posting/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $sql = "DELETE FROM posts WHERE id_post=:id";
+    $stmt = $this->db->prepare($sql);
+    
+    $data = [
+        ":id" => $id
+    ];
+
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
+});
+
